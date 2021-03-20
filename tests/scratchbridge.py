90f332cf26d8fbbc06d177de8725e8f6dc101649
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-03-20 08:32:34 -0600

    Update scratchbridge.py

--- tests/scratchbridge.py
+++ tests/scratchbridge.py
@@ -1,11 +1,17 @@
 import asyncio
 import websockets
+import os
+
+headers = {
+  'Cookie': f'scratchsessionid={os.getenv("sessionid")}',
+  'Origin': 'https://scratch.mit.edu'
+}
 async def send():
   data = ['{"name": "handshake", "user": "ninjaMAR", "project_id": 438906671}','{"method":"set","user":"ninjaMAR","project_id":"438906671","name":"☁ Test3","value":123456789}']
-  async with websockets.connect('wss://clouddata.scratch.mit.edu/') as websocket: 
+  async with websockets.connect('wss://clouddata.scratch.mit.edu/',extra_headers=headers) as websocket: 
     for i in data:
-      await websocket.send(eval(i))
-      response = websocket.recv()
-      print(response)
+      await websocket.send(i)
+      #response = await websocket.recv()
+      #print(response)
 
 asyncio.get_event_loop().run_until_complete(send())
