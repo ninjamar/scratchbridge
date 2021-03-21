9daf4d0b0b9b7f600003695725ba6927c31e1dca
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-03-21 08:16:48 -0600

    Update scratchbridge.py

--- tests/scratchbridge.py
+++ tests/scratchbridge.py
@@ -8,10 +8,10 @@ headers = {
 }
 async def send():
   data = ['{"name": "handshake", "user": "ninjaMAR", "project_id": 438906671}','{"method":"set","user":"ninjaMAR","project_id":"438906671","name":"☁ Test3","value":123456789}']
-  async with websockets.connect('wss://clouddata.scratch.mit.edu/',extra_headers=headers) as websocket: 
+  async with websockets.connect('wss://clouddata.scratch.mit.edu/',extra_headers=headers,ping_interval=None) as websocket: 
     for i in data:
       await websocket.send(i)
-      #response = await websocket.recv()
-      #print(response)
+      response = await websocket.recv()
+      print(response)
 
 asyncio.run(send())
