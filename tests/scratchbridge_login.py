84e253a7ad928949c08b4100576d7ed38cbbb08d
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-04-03 16:15:05 -0600

    Create scratchbridge_login.py

---
+++ tests/scratchbridge_login.py
@@ -0,0 +1,18 @@
+import asyncio
+import websockets
+import os
+from login import login
+r,csrf = login('ninjaMAR',os.getenv('password'))
+headers = {
+  'Cookie': [f'scratchsessionid={r['Set-Cookie'].split('=')[1]}'],
+  'Origin': 'https://scratch.mit.edu'
+}
+async def send():
+  data = ['{"name": "handshake", "user": "ninjaMAR", "project_id": 438906671}','{"method":"set","user":"ninjaMAR","project_id":"438906671","name":"☁ Test3","value":123456789}']
+  async with websockets.connect('wss://clouddata.scratch.mit.edu/',extra_headers=headers,ping_interval=None) as websocket: 
+    for i in data:
+      await websocket.send(i)
+      response = await websocket.recv()
+      print(response)
+
+asyncio.run(send())
